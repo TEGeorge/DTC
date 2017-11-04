2b1a931e04676cc64ca15286a782c916370dd813
TEGeorge <thomas-george@live.co.uk>
2017-11-04 10:13:43 +0000

    Prototype 4 Inital Commit - Refactored further, added legal language and restructure

---
+++ contracts/DPC.sol
@@ -0,0 +1,58 @@
+pragma solidity ^0.4.11;
+
+contract DataPolicy {
+
+    //States: Init, Accepted, Void, Dissolved
+
+    bytes32 identifier;
+    bytes32 hash;
+    bytes32 location;
+
+    address controller;
+    address processor;
+
+    struct Agreement {
+        address identity;
+        bytes32 name;
+    }
+
+    Agreement[] public agreements;
+
+}
+
+contract Agreements {
+
+    //States: Offered, Binding, Void, Dissolved
+
+    address policy;
+
+    address signatory;
+
+    
+}
+
+contract Obligations {
+    
+    address policy;
+
+    bytes32 identifier;
+    bytes32 hash;
+    bytes32 location;
+
+    struct ObligationEnforcement {
+        address agreement;
+        address enforcement;
+    }
+
+}
+
+contract ObligationEnforcement {
+
+    //State: Dispute, Dissolved, Enforced
+
+    address obligation;
+
+    uint damages;
+
+    
+}
